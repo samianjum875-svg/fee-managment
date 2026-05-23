f956590798336423cf2a6cf52ab1a210d548cb8d
axis <axis-limited@users.noreply.huggingface.co>
2026-05-23 10:55:40 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir -p /tmp/django_sessions/
+python manage.py shell -c "from django.contrib.auth import get_user_model; User=get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')"
 set -e
 
 echo "Running migrations on public schema..."
